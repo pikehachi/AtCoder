3958d34842a2180bb43755e54fc3d0cdd93d3861
pike8 <info@pike8.com>
2023-05-27 18:41:57 +0900

    add project

---
+++ practice/1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+struct Fast {Fast() {std::cin.tie(0); ios::sync_with_stdio(false);}} fast;
+// clang-format on
+#define int long long
+
+signed main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    string s;
+    cin >> s;
+    cout << a + b + c << " " << s << '\n';
+}
